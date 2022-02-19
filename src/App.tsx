748a1281f844e89b5bae6de000147f5ce9eaaf4f
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-19 13:02:51 +0000

    chore: more decorators

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { useEffect } from "react";
 import { Product } from "./Product/Product";
 import "./styles.css";
 
 export default function App() {
-  const phillips = new Product("light");
-  console.log(phillips);
+  useEffect(() => {
+    const phillips = new Product("light");
+    console.log(phillips);
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div id="app" />
     </div>
   );
 }
--- src/Product/Product.tsx
+++ src/Product/Product.tsx
@@ -1,9 +1,32 @@
-function Logger(constructor: Function) {
-  console.log("inside log");
-  console.log(constructor);
+// example 1
+// function Logger(constructor: Function) {
+//   console.log("inside log");
+//   console.log(constructor);
+// }
+
+// example 2 using Factory
+function Logger1(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+//example 3
+export function withTemplate(template: string, hookId: string) {
+  // _ tells typescript that I receive an arg but I'm not interested in it.
+  return function (_: Function) {
+    console.log("***", document.getElementById(hookId));
+    const hookEl = document.getElementById(hookId);
+    console.log(hookEl);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  };
+}
+
+// @Logger
+@Logger1("a product meta data")
 export class Product {
   name: string;
 
