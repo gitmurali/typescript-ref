5184a4ae86fc54996c50444aaa4f9a9c780e18f1
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-23 22:47:31 +0000

    Required

--- src/MappedTypes.ts
+++ src/MappedTypes.ts
@@ -1,6 +1,6 @@
 /******* Record type *******/
 
-type Person = { name: string; age: number; company: string };
+type Person = { name: string; age: number; company?: string };
 type ValidPerson = { name: boolean; age: boolean; company: string };
 type ValidP = Record<keyof Person, boolean>;
 /******* end of Record type *******/
@@ -9,4 +9,9 @@ type ValidP = Record<keyof Person, boolean>;
 type Person1 = Readonly<Person>;
 let a: Person1;
 a.age = 10;
+/********* end of Readonly **********/
+
+/******** Required type ************/
+type Person2 = Required<Person>;
+let a: Person2;
 /********* end of Readonly **********/
