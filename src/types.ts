0194b9ac7e6d3dbc47dd0aa5f7c25284c59ab037
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-23 09:13:29 +0000

    keyof types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+/******* keyof *********/
 interface Person {
   name: string;
   age: number;
@@ -8,3 +9,25 @@ interface Person {
 
 type PersonKeys = keyof Person;
 let a: PersonKeys = "name";
+
+/******* typeof *********/
+
+let env = {
+  server: "url",
+  port: 8080
+};
+
+type Env = typeof env;
+
+/******* keyof  *********/
+
+type Book = {
+  name: string;
+  title: string;
+  price: bigint;
+  published: number;
+};
+
+type ValuesOf<T> = T[keyof T];
+
+type R = ValuesOf<Book>;
