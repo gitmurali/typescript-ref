20239e620ebf07aafd218aca0a105ce0899a18ae
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-25 17:42:40 +0000

    union to intersection

--- src/distributive-types.ts
+++ src/distributive-types.ts
@@ -18,3 +18,14 @@ type keyOfAll<T> = T extends T ? keyof T : never;
 type R = keyof Person; // only name is retrieved.
 type B = keyOfAll<Person>; // all keys are retrieved.
 /***************************************/
+
+/**********  convert union to intersection*********/
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
+type FunctionUnion = (() => void) | ((p: string) => void);
+type SynthesizedFunctionIntersection = UnionToIntersection<FunctionUnion>;
+/********************************************************/
