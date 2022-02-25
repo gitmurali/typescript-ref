3289522e27bf07109fc0809f15f7da737ee96967
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-25 11:46:28 +0000

    keyOfAll

--- src/distributive-types.ts
+++ src/distributive-types.ts
@@ -7,3 +7,14 @@ enum E {
 }
 
 type K = MakeArray<string | E | boolean>;
+
+/*********** keyof *************/
+
+export type Person =
+  | { name: string; field: string }
+  | { name: string; specialty: string };
+
+type keyOfAll<T> = T extends T ? keyof T : never;
+type R = keyof Person; // only name is retrieved.
+type B = keyOfAll<Person>; // all keys are retrieved.
+/***************************************/
