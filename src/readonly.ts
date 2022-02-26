dad1725acdc253a630ce1dd2b5789832d74c0880
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-26 19:17:11 +0000

    retain some props

--- src/readonly.ts
+++ src/readonly.ts
@@ -4,6 +4,7 @@ type SomeEnv = {
     day: number;
     time: number;
   };
+  start: Date;
   restApi: { server: string; port: number };
   dbServers?: Array<{ server: string; port: number }>;
 };
@@ -12,7 +13,9 @@ type R = Readonly<SomeEnv>;
 
 // {} & is the trick to expand complete types
 type DeepReadonly<T> = {} & {
-  readonly [P in keyof T]: T[P] extends (...a: infer P) => infer R
+  readonly [P in keyof T]: T[P] extends Date
+    ? Date // retain date
+    : T[P] extends (...a: infer P) => infer R
     ? (...a: P) => DeepReadonly<R>
     : DeepReadonly<T[P]>;
 };
