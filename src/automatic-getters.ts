da9f96b3fd6a4cfd21258e236e04e18203350337
Murali Nutalapati <murali.nutalapatii@gmail.com>
2022-02-26 19:33:36 +0000

    setters

--- src/automatic-getters.ts
+++ src/automatic-getters.ts
@@ -6,7 +6,10 @@ type ServerEnv = {
 
 type EnvData = {
   [P in keyof ServerEnv as `get${Capitalize<P>}`]: () => ServerEnv[P];
-};
+} & {
+    [P in keyof ServerEnv as `set${Capitalize<P>}`]: (value: ServerEnv[P]) => void;
+  }
+
 
 type isAdmin = "isAdmin";
 type getisAdmin = `get${isAdmin}`;
