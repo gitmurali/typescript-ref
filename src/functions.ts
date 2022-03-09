3043f37c138679328364c5e3b296c4073662a207
Murali N <murali.nutalapatii@gmail.com>
2022-03-09 12:41:27 +0000

    construct signatures

--- src/functions.ts
+++ src/functions.ts
@@ -7,3 +7,12 @@ type TwoNumberCalc = (x: number, y: number) => number;
 
 const add: TwoNumberCalculation = (a, b) => a + b;
 const subtract: TwoNumberCalc = (x, y) => x - y;
+
+// construct signatures
+interface DateConstructor {
+  new (value: number): Date;
+}
+
+let MyDateConstructor: DateConstructor = Date;
+const d = new MyDateConstructor();
+//************* end of construct signatures **************/
