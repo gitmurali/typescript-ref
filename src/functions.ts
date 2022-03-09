a214bd64625724ea76c1ddbf9da703ceab44e790
Murali N <murali.nutalapatii@gmail.com>
2022-03-09 12:32:55 +0000

    example function type

--- src/functions.ts
+++ src/functions.ts
@@ -4,3 +4,6 @@ interface TwoNumberCalculation {
 }
 
 type TwoNumberCalc = (x: number, y: number) => number;
+
+const add: TwoNumberCalculation = (a, b) => a + b;
+const subtract: TwoNumberCalc = (x, y) => x - y;
