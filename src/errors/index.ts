3b32b4bd65cb0a0d4455bfe044af315752c843d2
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-03-16 22:01:01 +0000

    handle errors

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -6,3 +6,15 @@ const num1: number = 5;
 
 // @ts-ignore is like ignore any kind of error.
 // @ts-expect-error is like expect error and ignore that error
+
+function somethingRisky() {}
+
+try {
+  somethingRisky();
+} catch (err) {
+  if (err instanceof Error) {
+    console.log(err.stack);
+  } else {
+    console.log(err);
+  }
+}
