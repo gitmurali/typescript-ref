09894b6e9ddb432f34d0a98b80f7f511cb694e7b
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-03-16 22:13:17 +0000

    unknown vs any

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -8,13 +8,17 @@ const num1: number = 5;
 // @ts-expect-error is like expect error and ignore that error
 
 function somethingRisky() {}
-
+// https://stackoverflow.com/questions/51439843/unknown-vs-any
 try {
   somethingRisky();
 } catch (err) {
+  //err: unknown I don't know yet i have to figure it out yet.
   if (err instanceof Error) {
     console.log(err.stack);
   } else {
     console.log(err);
   }
 }
+
+// unknown is like I don't know yet
+// any is like I don't care
