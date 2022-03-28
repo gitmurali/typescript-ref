c9c69721eee185242f589dcfedabac9d90fe52a4
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-03-28 22:52:51 +0100

    get enum names

--- src/enum-to-string.ts
+++ src/enum-to-string.ts
@@ -10,3 +10,21 @@ let green: string = Color[Color.Green];
 let color: Color = Color[green as keyof typeof Color];
 
 console.log(color);
+
+// get names of enum
+
+enum Enum {
+  A
+}
+
+let nameOfA = Enum[Enum.A]; // "A"
+
+// Another way
+enum myEnum {
+  bar,
+  foo
+}
+
+for (var enumMember in myEnum) {
+  console.log("enum member: ", enumMember);
+}
