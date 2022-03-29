524b429d290f1dc9af7a7375af2c32f07dbc105d
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-03-29 10:10:21 +0100

    declare

---
+++ src/declare.ts
@@ -0,0 +1,4 @@
+// declare means like telling typescript compiler that "trust me, this variable exists and has this type"
+// You can use declare to tell the compiler about types before you write any implementation code and TypeScript will be happy.
+
+declare function foo(name: string): string;
