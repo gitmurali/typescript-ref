58de20640b336a8552f073f887017462f16abec1
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-03-29 10:15:17 +0100

    different ways of creating or initialising the objects.

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -8,3 +8,33 @@ const user02 = <User>{};
 
 user01.Email = "murali@abc.com";
 console.log(user02);
+
+interface IModal {
+  content: string;
+  form: string;
+  href: string;
+}
+
+// declaring objects
+declare const modal: IModal;
+
+//(or)
+const modal1: IModal = {
+  content: "",
+  form: "",
+  href: ""
+};
+
+//(or)  just lie to typescript
+const modal2 = {} as IModal;
+
+//(or)
+class Modal4 implements IModal {
+  content!: string;
+  form!: string;
+  href!: string;
+}
+
+const modal5 = new Modal4();
+const modal6: Modal4 = new Modal4();
+console.log(modal6);
