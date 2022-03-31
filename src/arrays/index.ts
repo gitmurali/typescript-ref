0027645b20b513e1f639ae823fa675accbe3a16a
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-03-31 15:24:31 +0100

    is typed array

--- src/arrays/index.ts
+++ src/arrays/index.ts
@@ -2,3 +2,22 @@
 type abc = [string | number, boolean][];
 const xyz: abc = [["Murali", true]];
 console.log(xyz);
+
+/**
+ * Check whether a given value is an array where
+ * each member is of a specified type
+ *
+ * @param arr - array to check
+ * @param check - type guard to use when evaluating each item
+ * @public
+ */
+export function isTypedArray<T>(
+  arr: unknown,
+  check: (x: any) => x is T
+): arr is T[] {
+  if (!Array.isArray(arr)) return false;
+  if (arr.some((item) => !check(item))) return false;
+  return true;
+}
+
+isTypedArray([1, 2, 3], (x): x is number => typeof x === "number");
