59a23cb401633d1afa8d82e91025d2f476286b57
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-04-01 14:47:52 +0100

    enum types

--- src/enums.ts
+++ src/enums.ts
@@ -6,3 +6,14 @@ enum Colors {
 
 let red: string = Colors[Colors.RED]; //string RED
 console.log(red);
+
+enum Colors2 {
+  RED,
+  GREEN,
+  BLUE
+}
+
+let green = Colors2[1];
+let red1 = Colors2[0];
+
+console.log(green, red1);
