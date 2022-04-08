1fefaa8674c81b7400af5f45e47d1c2eccc745aa
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-04-08 10:12:45 +0100

    type casting

--- src/string/string.ts
+++ src/string/string.ts
@@ -3,3 +3,11 @@ var b: String = new String("another test");
 
 console.log(typeof a); // string
 console.log(typeof b); // object
+
+type Fruits = "Orange" | "Apple" | "Banana";
+
+const myString: string = "Banana";
+
+const MyFruits: Fruits = myString as Fruits;
+
+console.log(MyFruits);
