d2f3a3c378b6f1ba7f039fdbe717193d11f98fa1
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-04-21 22:59:59 +0100

    console log order

--- src/Quiz/index.ts
+++ src/Quiz/index.ts
@@ -26,3 +26,27 @@ let second: String & Number; //
 first = "abc";
 second = "abc";
 second = new String("abc");
+
+// example 4
+
+// In what order will the animal names below be printed to the console?
+// dog, cat, elephant, giraffe, zebra, koala, lion, moose
+function getData() {
+  console.log("elephant");
+  const p = new Promise((resolve) => {
+    console.log("giraffe");
+    resolve("lion");
+    console.log("zebra");
+  });
+  console.log("koala");
+  return p;
+}
+async function main() {
+  console.log("cat");
+  const result = await getData();
+  console.log(result);
+}
+console.log("dog");
+main().then(() => {
+  console.log("moose");
+});
