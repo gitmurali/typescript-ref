1619d79adad3c2e5bd63ca318f927b1e6c4e3cd0
Murali ⚡️ <murali.nutalapatii@gmail.com>
2022-04-21 22:33:42 +0100

    string to object

--- src/Quiz/index.ts
+++ src/Quiz/index.ts
@@ -2,3 +2,18 @@ export class Person {
   #name = ""; // javascript private field
   private age = 1; // typescript private field.
 }
+
+//example 2
+
+const str = "hello"; // convert string to object.
+let val = { ...str.split("") };
+console.log(val);
+/**
+ * {
+ *   '0': 'h',
+ *   '1': 'e',
+ *   '2': 'l',
+ *   '3': 'l',
+ *   '4': 'o'
+ * }
+ */
